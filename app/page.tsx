91a4c93f1acdb99de0e9f6ed31d3043e16b968a1
iDmitryi <hdumitru1998@gmail.com>
2024-03-05 23:04:50 +0200

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      heloo
+      <p className="text-3xl font-medium text-sky-700">world</p>
+      <Button variant="destructive"> Click</Button>
+    </>
+  )
+}
