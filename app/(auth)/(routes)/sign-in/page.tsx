3df124562552521c9aa703ad6e6a62f3541ed53b
Dumitru Hanganu <dumitru.hanganu@ivfuture.uk>
2024-03-06 19:23:01 +0200

    add authentication

--- app/(auth)/(routes)/sign-in/page.tsx
+++
@@ -1,5 +0,0 @@
-const SignIn = () => {
-  return <p>Un Protected</p>
-}
-
-export default SignIn
--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,10 +1,10 @@
 import { Button } from '@/components/ui/button'
-import Image from 'next/image'
-
+import { UserButton } from '@clerk/nextjs'
 export default function Home() {
   return (
     <>
       <p>Protected</p>
+      <UserButton afterSignOutUrl="/sign-in" />
       <p className="text-3xl font-medium text-sky-700">world</p>
       <Button variant="destructive"> Click</Button>
     </>
