97a978233960269b16d66eaa81952b547c9dc24f
iDmitryi <hdumitru1998@gmail.com>
2024-03-15 00:16:36 +0200

    implement category form

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/category-form.tsx
@@ -0,0 +1,125 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import axios from 'axios'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '@/components/ui/form'
+
+import { Button } from '@/components/ui/button'
+import { Textarea } from '@/components/ui/textarea'
+import { cn } from '@/lib/utils'
+import { Pencil } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import toast from 'react-hot-toast'
+import { Course } from '@prisma/client'
+import { Combobox } from '@/components/ui/combobox'
+
+interface CategoryFormProps {
+  initialData: Course
+  courseId: string
+  options: { label: string; value: string }[]
+}
+
+const formSchema = z.object({
+  categoryId: z.string().min(1),
+})
+
+const CategoryForm = ({
+  initialData,
+  courseId,
+  options,
+}: CategoryFormProps) => {
+  const [isEditing, setIsEditing] = useState(false)
+  const toggleEdit = () => setIsEditing(prevState => !prevState)
+  const router = useRouter()
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      categoryId: initialData?.categoryId || '',
+    },
+  })
+
+  const { isSubmitting, isValid } = form.formState
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.patch(`/api/courses/${courseId}`, values)
+      toast.success('Course updated')
+      toggleEdit()
+      router.refresh()
+    } catch (error) {
+      toast.error('Something went wrong')
+    }
+  }
+
+  const selectedOption = options.find(
+    option => option.value === initialData.categoryId
+  )
+
+  console.log({ selectedOption })
+
+  return (
+    <div className="mt-7 border bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        Course category
+        <Button onClick={toggleEdit} variant="ghost">
+          {isEditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <Pencil className="h-4 w-4 mr-2" />
+              Edit category
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && (
+        <p
+          className={cn(
+            'text-sm mt-2',
+            !initialData.categoryId && 'text-slate-500 italic'
+          )}
+        >
+          {selectedOption?.label || 'No category'}
+        </p>
+      )}
+      {isEditing && (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 mt-4"
+          >
+            <FormField
+              control={form.control}
+              name="categoryId"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Combobox options={options} {...field} />
+                    {/* <ComboboxDemo /> */}
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex item-center gap-x-2">
+              <Button disabled={isSubmitting || !isValid}>Save</Button>
+            </div>
+          </form>
+        </Form>
+      )}
+    </div>
+  )
+}
+
+export default CategoryForm
