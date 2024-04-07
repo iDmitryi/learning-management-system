1ba3236d4d184d02655e8bcf2a50b79e8051ae74
Dumitru Hanganu <dumitru.hanganu@ivfuture.uk>
2024-04-08 00:10:33 +0300

    auto play next video

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
@@ -1,9 +1,13 @@
 'use client'
 
-import { cn } from '@/lib/utils'
+import { useConfettiStore } from '@/hooks/use-confetti-store'
+import { cn, texts } from '@/lib/utils'
 import MuxPlayer from '@mux/mux-player-react'
+import axios from 'axios'
 import { Loader2, Lock } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 import { useState } from 'react'
+import toast from 'react-hot-toast'
 
 interface VideoPlayerProps {
   playbackId: string
@@ -25,6 +29,33 @@ export const VideoPlayer = ({
   title,
 }: VideoPlayerProps) => {
   const [isReady, setIsReady] = useState(false)
+  const router = useRouter()
+  const confetti = useConfettiStore()
+
+  const onEnd = async () => {
+    try {
+      if (completeOnEnd) {
+        await axios.put(
+          `/api/courses/${courseId}/chapters/${chapterId}/progress`,
+          {
+            isCompleted: true,
+          }
+        )
+
+        if (!nextChapterId) confetti.onOpen()
+
+        toast.success('Progress updated')
+
+        if (nextChapterId)
+          router.push(`/courses/${courseId}/chapters/${nextChapterId}`)
+
+        router.refresh()
+      }
+    } catch (error) {
+      toast.error(texts.error)
+    }
+  }
+
   return (
     <div className="relative aspect-video">
       {!isLocked && !isReady && (
@@ -43,7 +74,7 @@ export const VideoPlayer = ({
           title={title}
           className={cn(!isReady && 'hidden')}
           onCanPlay={() => setIsReady(true)}
-          onEnded={() => {}}
+          onEnded={onEnd}
           autoPlay
           playbackId={playbackId}
         />
