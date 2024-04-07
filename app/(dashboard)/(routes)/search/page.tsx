8a89d2308aeb2cd2cdc49e1669273aa2862f0890
Dumitru Hanganu <dumitru.hanganu@ivfuture.uk>
2024-04-07 16:12:29 +0300

    update prisma & display student courses

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,14 +1,30 @@
 import { db } from '@/lib/db'
 import { Categories } from './_components/categories'
 import { SearchInput } from '@/components/search-input'
+import { getCourses } from '@/actions/get-courses'
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+import { CoursesList } from '@/components/courses-list'
 
-const SearchPAge = async () => {
+interface SearchPageProps {
+  searchParams: {
+    title: string
+    categoryId: string
+  }
+}
+const SearchPage = async ({ searchParams }: SearchPageProps) => {
+  const { userId } = auth()
+  if (!userId) return redirect('/')
+
   const categories = await db.category.findMany({
     orderBy: {
       name: 'asc',
     },
   })
-
+  const courses = await getCourses({
+    userId,
+    ...searchParams,
+  })
   return (
     <>
       <div className="px-6 pt-6 md:hidden md:mb-0 block">
@@ -17,8 +33,9 @@ const SearchPAge = async () => {
       <div className="p-6">
         <Categories items={categories} />
       </div>
+      <CoursesList items={courses} />
     </>
   )
 }
 
-export default SearchPAge
+export default SearchPage
