1b94c770fd847435fc5891eea43d4ee43aedb3ac
Dumitru Hanganu <dumitru.hanganu@ivfuture.uk>
2024-04-07 15:20:25 +0300

    search query implementation

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,5 +1,6 @@
 import { db } from '@/lib/db'
 import { Categories } from './_components/categories'
+import { SearchInput } from '@/components/search-input'
 
 const SearchPAge = async () => {
   const categories = await db.category.findMany({
@@ -9,9 +10,14 @@ const SearchPAge = async () => {
   })
 
   return (
-    <div className="p-6">
-      <Categories items={categories} />
-    </div>
+    <>
+      <div className="px-6 pt-6 md:hidden md:mb-0 block">
+        <SearchInput />
+      </div>
+      <div className="p-6">
+        <Categories items={categories} />
+      </div>
+    </>
   )
 }
 
