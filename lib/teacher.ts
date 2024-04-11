4b4e250a79eed36eba7502051861611a4d6d7fa6
Dumitru Hanganu <dumitru.hanganu@ivfuture.uk>
2024-04-11 21:50:24 +0300

    implement teacher mode for everyoane

--- lib/teacher.ts
+++ lib/teacher.ts
@@ -1,3 +1,6 @@
 export const isTeacher = (userId?: string | null) => {
-  return userId === process.env.NEXT_PUBLIC_TEACHER_ID
+  return (
+    process.env.NEXT_PUBLIC_TEACHER_MODE ||
+    userId === process.env.NEXT_PUBLIC_TEACHER_ID
+  )
 }
