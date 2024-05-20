bfa39df91bd72aa7877494ca72eb2626a7062937
iDmitryi <hdumitru1998@gmail.com>
2024-05-20 21:30:22 +0300

    fixed mobile z-index for mobile nav bar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -4,10 +4,10 @@ import Sidebar from './_components/sidebar'
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full">
-      <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-49 bg-white dark:bg-zinc-900">
+      <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full bg-white dark:bg-zinc-900 z-50">
         <Navbar />
       </div>
-      <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0">
+      <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
         <Sidebar />
       </div>
       <main className="md:pl-56 pt-[80px] h-full">{children}</main>
