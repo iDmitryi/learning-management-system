747648ea6f39f795602e983c259e02ee8a0d15a1
iDmitryi <hdumitru1998@gmail.com>
2024-05-20 21:39:49 +0300

    rename exit button

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -33,7 +33,7 @@ export const NavbarRoutes = () => {
           <Link href="/">
             <Button size="sm" variant="ghost">
               <LogOut className="h-4 w-4 mr-2" />
-              Exit
+              Student Mode
             </Button>
           </Link>
         ) : isTeacher(userId) ? (
